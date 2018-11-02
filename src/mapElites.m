110a402f6924f31597d4b88f22a8879a31624d4e
adam <adam.ga@gmail.com>
2018-11-02 13:55:28 +0100

    Refactored initializing population

--- src/mapElites.m
+++ src/mapElites.m
@@ -1,13 +1,13 @@
-function [map, percImproved, h, evalTime] = mapElites(fitnessFunction,map,p,d)
+function [map,record] = mapElites(domain, varargin)
 %mapElites - Multi-dimensional Archive of Phenotypic Elites algorithm
 %
-% Syntax:  map = mapElites(fitnessFunction, map, p, d);
+% Syntax:  map = mapElites(problemDomain, hyperparameters, map);
 %
 % Inputs:
-%   fitnessFunction - funct  - returns fitness of vector of individuals
-%   map             - struct - initial solutions in F-dimensional map
-%   p               - struct - Hyperparameters for algorithm, visualization, and data gathering
-%   d               - struct - Domain definition
+%   fitnessFunction - [funct ] - returns fitness of vector of individuals
+%   map             - [struct] - initial solutions in F-dimensional map
+%   p               - [struct] - Hyperparameters for algorithm, visualization, and data gathering
+%   d               - [struct] - Domain definition
 %
 % Outputs:
 %   map    - struct - population archive
@@ -15,27 +15,43 @@ function [map, percImproved, h, evalTime] = mapElites(fitnessFunction,map,p,d)
 %   h      - [1X2]  - axes handle, data handle
 %
 %
-% See also: createChildren, getBestPerCell, updateMap
 
 % Author: Adam Gaier
-% Bonn-Rhein-Sieg University of Applied Sciences (HBRS)
-% email: adam.gaier@h-brs.de
-% Jun 2016; Last revision: 17-Oct-2017
+% Bonn-Rhein-Sieg University of Applied Sciences (BRSU)
+% Inria Nancy - Grand Est
+% email: adam.gaier@{h-brs.de, inria.fr}
+% Nov 2018; Last revision: 02-Nov-2018
 
 % TODO:
-% parser instead of the sloppy ifs at top
+% * Write todo list
+% * Complete todo list
+
+%------------- Input Parsing ------------
+parse = inputParser;
+parse.addRequired('domain');
+parse.addOptional('startMap', []);
+parse.addOptional('genPerVis', 2^3);
+parse.addOptional('gifMap', false);
+
+parse.parse(domain,varargin{:});
+d        = parse.Results.domain;
+startMap = parse.Results.map;
+visMod   = parse.Results.genPerVis;
+gifMap   = parse.Results.gifMap;
 
 %------------- BEGIN CODE --------------
-% View Initial Map
-h = [];
-if p.display.illu
-    figure(2); clf;
-    [h(1), h(2)] = viewMap(map.fitness, d, map.edges); title('Illumination Fitness')
+%% Fill initial map
+if ~isempty(startMap)
+    map = startMap;
+else
+    map = createMap(d.mapDims, d.sampleInd);
+    startPop = feval(d.randInd, d.nInitial,d.recombine);
+    [fitness, behavior, misc, startPop] = feval(d.evaluate, startPop, d); 
+    map = addToMap(map, startPop, fitness, behavior, misc);    
 end
 
 %% MAP-Elites
-evalTime = 0;
-iGen = 1;
+evalTime = 0; iGen = 1;
 while (iGen <= p.nGens)
     %% 1) Create and Evaluate Children
     % Create children which satisfy geometric constraints for validity
